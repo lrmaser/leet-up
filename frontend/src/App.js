12841155a0bb1b24dbaa7f11dc529f459f62d070
Laura <laura.maser3@yahoo.com>
2022-02-04 17:24:32 -0500

    Created Navigation component with ProfileButton.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import * as sessionActions from './store/session';
+import Navigation from './components/Navigation';
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from './components/SignupFormPage';
 
@@ -14,15 +15,20 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
-    <Switch>
-      <Route path='/login'>
-        <LoginFormPage />
-      </Route>
-      <Route path='/signup'>
-        <SignupFormPage />
-      </Route>
-    </Switch>
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path='/login'>
+            <LoginFormPage />
+          </Route>
+          <Route path='/signup'>
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import * as sessionActions from '../../store/session';
+
+const ProfileButton = ({ user }) => {
+  const dispatch = useDispatch();
+  const [ showMenu, setShowMenu ] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+      <button className='user-icon-button' onClick={openMenu}>
+        <div className='user-icon'>
+          <i class="fas fa-user-circle"></i>
+        </div>
+      </button>
+      {showMenu && (
+        <ul className='profile-dropdown'>
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default ProfileButton;
