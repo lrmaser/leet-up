02a6d66663af7e1f825b4b800ea28032393c368e
Laura <laura.maser3@yahoo.com>
2022-02-04 13:20:09 -0500

    Created css file for LoginForm.

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import * as sessionActions from '../../store/session';
+import './LoginForm.css';
 
 const LoginFormPage = () => {
   const dispatch = useDispatch();
