02eb21553bb60daebd52e5ba0c768f6964edcc4f
Laura <laura.maser3@yahoo.com>
2022-02-07 14:15:50 -0500

    Completed some styling on the signup page.

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -12,6 +12,7 @@ const SignupFormPage = () => {
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ confirmPassword, setConfirmPassword ] = useState('');
+  const [ age, setAge ] = useState(false);
   const [ errors, setErrors ] = useState([]);
 
   if (sessionUser) return (
@@ -32,48 +33,83 @@ const SignupFormPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Username
-        <input
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Email
-        <input
-          type='text'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type='password'
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <main className='signup-main'>
+      <div className='signup-form-container'>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+          </ul>
+          <div className='signup-headers'>
+            <img src='/images/LeetUpLogo.png' alt='LeetUp Logo'></img>
+            <h1>Sign Up</h1>
+          </div>
+          <div className='signup-form-contents'>
+            <div className='signup-name-container'>
+              <label for='signup-name'>Username</label>
+                <input
+                  type='text'
+                  id='signup-name'
+                  name='signup-name'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+            </div>
+            <div className='signup-email-container'>
+              <label for='signup-email'>Email Address</label>
+                <input
+                  type='text'
+                  id='signup-email'
+                  name='signup-email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder='example@email.com'
+                  required
+                />
+            </div>
+            <div className='signup-password-container'>
+              <label for='signup-password'>Password</label>
+                <input
+                  type='password'
+                  id='signup-password'
+                  name='signup-password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+            </div>
+            <div className='signup-confirm-password-container'>
+              <label for='signup-confirm-password'>Confirm Password</label>
+                <input
+                  type='password'
+                  id='signup-confirm-password'
+                  name='signup-confirm-password'
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+            </div>
+            <div className='signup-age-container'>
+              <label for='signup-age'>Age</label>
+                <input
+                  type='checkbox'
+                  id='signup-age'
+                  name='signup-age'
+                  value={age}
+                  onChange={(e) => setAge(e.target.checked)}
+                  required
+                />
+                <span>I am 18 years of age or older.</span>
+            </div>
+            <button type='submit' className='signup-form-button'>Sign Up</button>
+          </div>
+          <div className='signup-already-member'>
+            <span>Already a member?</span>
+            <a href='#'>Log In</a>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 };
 
