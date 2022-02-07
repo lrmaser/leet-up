3c41f6ba177322eff6f6987acbd69a089fa8807e
Laura <laura.maser3@yahoo.com>
2022-02-07 18:23:37 -0500

    Created EventForm component.

---
+++ frontend/src/components/EventFormPage/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import './EventForm.css';
+
+const EventFormPage = () => {
+  // useState
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Error handling
+  };
+
+  return null;
+};
+
+export default EventFormPage;
