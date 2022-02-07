f42afa487474baadf1e0c6bf5364456f69876375
Laura <laura.maser3@yahoo.com>
2022-02-07 16:30:14 -0500

    Migrated the Rsvps table.

---
+++ backend/db/models/rsvp.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Rsvp = sequelize.define('Rsvp', {
+    eventId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  }, {});
+  Rsvp.associate = function(models) {
+    // associations can be defined here
+  };
+  return Rsvp;
+};
