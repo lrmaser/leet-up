6b830b7f14991d6deae50eb401a7937c59a2cfb8
Laura <laura.maser3@yahoo.com>
2022-02-08 13:09:00 -0500

    Created backend routes for events.

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -13,6 +13,44 @@ router.get('/', asyncHandler(async (req, res) => {
   return res.json(events);
 }));
 
+// POST /api/events - CREATE
+router.post('/', asyncHandler(async (req, res) => {
+  const event = await Event.create(req.body);
+  return res.json(event);
+}));
+
+// DELETE /api/events/:eventId - DELETE
+router.delete('/api/events/:eventId(\\d+)', asyncHandler(async (req, res) => {
+  const eventId = req.params.eventId;
+  const event = await Event.findByPk(eventId);
+
+  if (!event) throw new Error('Cannot find event');
+
+  await Event.destroy({
+    where: {
+      id: event.id
+    }
+  });
+
+  return res.json(event.id);
+}));
+
+// PUT /api/events/:eventId - UPDATE
+router.put('/api/events/:eventId(\\d+)', asyncHandler(async (req, res) => {
+  const eventId = req.params.eventId;
+  const event = await Event.findByPk(eventId);
+
+  if (!event) throw new Error('Cannot find event');
+
+  await Event.update({
+    where: {
+      id: event.id
+    }
+  });
+
+  return res.json(event.id);
+}));
+
 
 
 module.exports = router;
