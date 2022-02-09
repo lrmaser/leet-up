c87a1c20a8a5b45c4612de514c663e27356442e6
Laura <laura.maser3@yahoo.com>
2022-02-09 00:00:30 -0500

    Styled login modal.

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import * as sessionActions from '../../store/session';
 
@@ -24,25 +25,39 @@ const LoginForm = () => {
       <ul>
         {errors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
-      <label>
-        Email
-        <input
-          type='text'
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type='submit'>Log In</button>
+      <div className='login-headers'>
+        <img src='/images/LeetUpLogo.png' alt='LeetUp Logo'></img>
+        <h1>Log In</h1>
+      </div>
+      <div className='login-form-contents'>
+        <div className='login-email-container'>
+          <label htmlFor='login-email'>Email</label>
+            <input
+              type='text'
+              id='login-email'
+              name='login-email'
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+        </div>
+        <div className='login-password-container'>
+          <label htmlFor='login-password'>Password</label>
+            <input
+              type='password'
+              id='login-password'
+              name='login-password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+        </div>
+        <button type='submit' className='login-form-button'>Log In</button>
+      </div>
+      <div className='login-not-member'>
+        <span>Not a member yet?</span>
+        <Link to='/signup'>Sign Up</Link>
+      </div>
     </form>
   );
 };
