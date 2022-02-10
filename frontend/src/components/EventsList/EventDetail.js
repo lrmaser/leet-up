7810ed4931f9b32b5678108ede59a74bde55f670
Laura <laura.maser3@yahoo.com>
2022-02-10 11:46:19 -0500

    Wrapped EventDetail component in a link to navigate to event details pages.

--- frontend/src/components/EventsList/EventDetail.js
+++ frontend/src/components/EventsList/EventDetail.js
@@ -1,4 +1,5 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 const formatDate = (date) => {
   const dateString = new Date(date).toDateString();
   const dateStringSplit = dateString.split(' ');
@@ -15,23 +16,25 @@ const formatTime = (time) => {
 
 const EventDetail = ({ id, image, date, eventName, groupName }) => {
   return (
-    <div className='event-list-detail'>
-      <div className='event-list-image-container'>
-        <img className='event-list-image' src={image} alt='Event'></img>
+    <Link to={`/events/${id}`}>
+      <div className='event-list-detail'>
+        <div className='event-list-image-container'>
+          <img className='event-list-image' src={image} alt='Event'></img>
+        </div>
+        <div className='event-list-info'>
+          <div className='event-list-date-container'>
+            <div className='event-list-date'>{`${formatDate(date)} @ ${formatTime(date)}`}</div>
+          </div>
+          <p className='event-list-event-name'>{eventName}</p>
+          <div className='event-list-group-name-container'>
+            <p className='event-list-group-name'>{groupName}</p>
+          </div>
+          <div className='event-list-attendees-container'>
+            <div className='event-list-attendees'>0 attendees</div>
+          </div>
+        </div>
       </div>
-      <div className='event-list-info'>
-        <div className='event-list-date-container'>
-          <div className='event-list-date'>{`${formatDate(date)} @ ${formatTime(date)}`}</div>
-        </div>
-        <p className='event-list-event-name'>{eventName}</p>
-        <div className='event-list-group-name-container'>
-          <p className='event-list-group-name'>{groupName}</p>
-        </div>
-        <div className='event-list-attendees-container'>
-          <div className='event-list-attendees'>0 attendees</div>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
 
