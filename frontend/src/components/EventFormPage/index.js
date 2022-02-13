0636ab56d153f65a4860b22ae90a3d9a8849ced1
Laura <laura.maser3@yahoo.com>
2022-02-13 17:30:28 -0500

    Styled errors on event form.

--- frontend/src/components/EventFormPage/index.js
+++ frontend/src/components/EventFormPage/index.js
@@ -69,14 +69,26 @@ const EventFormPage = () => {
     history.push('/events');
   };
 
+  let errorBox;
+  if (errors.length > 0) {
+    errorBox = (
+      <div className='event-form-errors'>
+        <p>The following error(s) occurred:</p>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+      </div>
+    );
+  } else {
+    errorBox = null;
+  }
+
   return (
     <main className='event-form-main'>
       <div className='event-form-container'>
         <form onSubmit={handleSubmit}>
           <h2>Create an Event</h2>
-          <ul className='event-form-errors'>
-            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-          </ul>
+          {errorBox}
           <div className='event-form-group-container'>
             <label htmlFor='event-form-group'>Hosting Group</label>
             <select
