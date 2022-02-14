cc30b4def1027ce4d2982169d3ceb8ec7e36c8d1
Laura <laura.maser3@yahoo.com>
2022-02-13 19:32:20 -0500

    Made it so clicking footer icons opens a new tab.

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -8,10 +8,10 @@ const Footer = () => {
       <div className='footer-content'>
         <div className='footer-header'>Created by Laura Maser</div>
         <div className='footer-icons'>
-          <a href='https://www.linkedin.com/in/laura-maser-225196b2/'>
+          <a href='https://www.linkedin.com/in/laura-maser-225196b2/' target='_blank' rel='noopener noreferrer'>
             <i className="fab fa-linkedin"></i>
           </a>
-          <a href='https://github.com/lrmaser'>
+          <a href='https://github.com/lrmaser' target='_blank' rel='noopener noreferrer'>
             <i className="fab fa-github"></i>
           </a>
         </div>
